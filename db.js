b913bcc2883ac8b64ed6de6cb334e263b4919949
Harikrishna-Challa <harichvk18@gmail.com>
2025-04-24 19:07:58 +0530

    added clever cloud mysql to resolve render issue which was mysql is not installed in rendor

--- db.js
+++ db.js
@@ -1,21 +1,19 @@
 const mysql = require("mysql2");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "bzjbs8cbdsiluxrclkg7-mysql.services.clever-cloud.com",         // from clever cloud
+  port: 3306,
+  user: "utwgfdhptctjihge",
+  password: "GxZnbsaVNdeyxqzGifo8",
+  database: "bzjbs8cbdsiluxrclkg7",
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Error connecting to the database:", err);
+    console.error("❌ Error connecting to the database:", err.message);
     return;
   }
-  console.log("MySQL connected...");
+  console.log("✅ Connected to Clever Cloud MySQL!");
 });
 
 module.exports = db;
